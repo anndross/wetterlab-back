6d6c011e144b2a5a743598d9e569e9e10b7e4c81
= <=>
2024-12-11 01:07:03 -0300

    chore: adjust path to apps

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import sys
+
+# Adiciona o diretório 'src' ao PYTHONPATH
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Diretório da `core`
+SRC_DIR = os.path.join(BASE_DIR, 'src')
+sys.path.append(SRC_DIR)
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Adiciona o diretório 'src' ao PYTHONPATH
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Diretório da `core`
+SRC_DIR = os.path.join(BASE_DIR, 'src')
+sys.path.append(SRC_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
