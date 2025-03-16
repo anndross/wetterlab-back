4d39025d54f1ccb179ce06dbacb5af458876f78d
Andrey Soares <andreysoares.dev@gmail.com>
2025-03-16 17:18:41 -0300

    chore: add logger

--- erp/views/login.py
+++ erp/views/login.py
@@ -1,28 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from setup.utils import parse_bson
+from setup.utils import parse_bson, logger
 from setup.utils.jwt import encode_jwt
 from ..serializers import LoginSerializer
 from ..services.login import LoginService
+from rest_framework.exceptions import ValidationError
 
 class LoginView(APIView):
 
     def post(self, request):
-        login_serializer = LoginSerializer(data=request.data)
+        try:
+            login_serializer = LoginSerializer(data=request.data)
+        except Exception:
+            logger.error(f"error: {login_serializer.errors['email'][0]}")
+            raise ValidationError(detail={ "error": login_serializer.errors['email'][0] }, code=status.HTTP_400_BAD_REQUEST)
+
 
         if not login_serializer.is_valid():
+            logger.error(f"error: {login_serializer.errors['email'][0]}")
             return Response({ "error": login_serializer.errors['email'][0] }, status=status.HTTP_400_BAD_REQUEST)
 
-        login_service = LoginService(login_serializer.validated_data.get('email'))
-        
-        customer = login_service.find_customer()
+        try:
+            login_service = LoginService(login_serializer.validated_data.get('email'))
+            customer = login_service.find_customer()
+        except Exception: 
+            logger.error("error: Não foi encontrado nenhum usuário com este email")
+            raise ValidationError(detail={ "error": "Não foi encontrado nenhum usuário com este email" }, code=status.HTTP_400_BAD_REQUEST)
 
         if not customer: 
             return Response({ "error": "Não foi encontrado nenhum usuário com este email" }, status=status.HTTP_400_BAD_REQUEST)
        
-        customer_json = parse_bson(customer)
-        auth_token = encode_jwt(customer_json)
+        try:
+            customer_json = parse_bson(customer)
+            auth_token = encode_jwt(customer_json)        
+        except Exception:
+            logger.error("error: Não foi encontrado nenhum usuário com este email")
+            raise ValidationError(detail={ "error": "Não foi encontrado nenhum usuário com este email" }, code=status.HTTP_400_BAD_REQUEST)
 
         return Response({
             "message": "Autenticado com sucesso!", 
--- setup/utils/logger.py
+++ setup/utils/logger.py
@@ -1,3 +1,16 @@
+import os
 import logging
 
-logger = logging.getLogger(__name__)
+# Garante que o log seja salvo na pasta do projeto
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../logs")
+os.makedirs(log_dir, exist_ok=True)
+
+# Configuração do logger
+file_handler = logging.FileHandler(os.path.join(log_dir, 'app.log'))
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(file_handler)
